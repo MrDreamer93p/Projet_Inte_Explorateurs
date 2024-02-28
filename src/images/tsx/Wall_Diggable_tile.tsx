15f4eb5d24698fd643f92aa6224b3d6b7de03a3e
WilX0 <129814502+WilX0@users.noreply.github.com>
2024-02-28 12:00:57 +0100

    Ajout TMX

---
+++ src/images/tsx/Wall_Diggable_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Wall_Diggable_tile" tilewidth="16" tileheight="16" tilecount="1080" columns="30">
+ <image source="../../Explorateurs_Tiles/Wall_Diggable.png" width="480" height="576"/>
+</tileset>
